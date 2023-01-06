d13fbd870708476f338902b8e069eee3085814ab
pravinterdalui <pravinterdal20@gmail.com>
2023-01-06 15:44:27 +0530

    Updated index.html. Resized image

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FileUploadComponent } from './component/file-upload/file-upload.component';
+import { FormsComponent } from './component/forms/forms.component';
+import { ReactiveComponent } from './component/reactive/reactive.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ReactiveComponent
+},
+
+{
+    path: 'form',
+    component: FormsComponent
+},
+{
+  path: 'file-upload',
+  component: FileUploadComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
